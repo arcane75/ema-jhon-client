6250788f2e87bf17901fceba74f9ecea394f132a
unknown <mdrakibulhoque83@gmail.com>
2021-10-28 21:21:23 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCnNLMf7bL59nILXiirZOTo6wj6moJEwuM",
+    authDomain: "emajhonsite.firebaseapp.com",
+    projectId: "emajhonsite",
+    storageBucket: "emajhonsite.appspot.com",
+    messagingSenderId: "102296104880",
+    appId: "1:102296104880:web:1fa51c72ebecc0ed37b075"
+  };
+
+export default firebaseConfig;
